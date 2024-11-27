37e92e18a432ad229efedfde51402ec9dec168cb
iampassos <78429887+iampassos@users.noreply.github.com>
2024-11-27 20:33:23 -0300

    feat: add dark mode toggle and page content

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 const buttons = document.querySelectorAll(".top-button");
+const normalTexts = document.querySelectorAll(".normal-text");
+const darkModeButton = document.querySelector("#mode");
+
+let dark = false;
+
+const toggleDark = function () {
+  document.body.classList.toggle("dark-mode");
+  normalTexts.forEach((p) => p.classList.toggle("dark-mode"));
+  buttons.forEach((b) => b.classList.toggle("dark-mode"));
+  const links = document.querySelectorAll(".link");
+  const name = document.querySelector(".name");
+
+  if (links) {
+    links.forEach((l) => l.classList.toggle("dark-mode"));
+  }
+
+  if (name) {
+    name.classList.toggle("dark-mode");
+  }
+
+  dark = dark ? false : true;
+
+  darkModeButton.textContent = dark ? "Light Mode" : "Dark Mode";
+};
 
 buttons.forEach((button) => {
   button.addEventListener("click", function (event) {
     event.preventDefault();
 
-    if (window.location.href !== button.href) {
+    const id = button.id;
+
+    if (id == "mode") {
+      toggleDark();
+    }
+
+    if (window.location.href !== button.href && id !== "mode") {
       window.location.href = this.getAttribute("href");
     }
   });
